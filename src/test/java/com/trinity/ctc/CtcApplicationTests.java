0d2f39ae6874303f4bd3c38e49cff63939371e12
GumaLee <rkdxhd97@naver.com>
2025-02-10 13:36:23 +0900

    CHORE - 프로젝트 초기 세팅
    - application.yml : common & local & prod
    - docker & docker-compose
    - build.gradle
    - gitignore

---
+++ src/test/java/com/trinity/ctc/CtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinity.ctc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
