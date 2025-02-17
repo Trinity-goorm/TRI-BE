252a2ea7272bf05112250ee6b3077d1c5a7697ea
GumaLee <rkdxhd97@naver.com>
2025-02-17 15:41:08 +0900

    TRI-60 각종 에러코드 생성

---
+++ src/main/java/com/trinity/ctc/util/exception/error_code/ReservationTimeErrorCode.java
@@ -0,0 +1,15 @@
+package com.trinity.ctc.util.exception.error_code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReservationTimeErrorCode implements ErrorCode {
+
+    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 예약시간을 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
