3cd329f7f2c37b88acab8c2ff48365e4278dc049
GumaLee <rkdxhd97@naver.com>
2025-02-18 13:40:11 +0900

    TRI-68 예약확정기능 관련 ErrorCode 추가

--- src/main/java/com/trinity/ctc/util/exception/error_code/ReservationErrorCode.java
+++ src/main/java/com/trinity/ctc/util/exception/error_code/ReservationErrorCode.java
@@ -8,7 +8,12 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ReservationErrorCode implements ErrorCode {
     PAYING_BY_ANOTHER(HttpStatus.BAD_REQUEST, "다른 사용자가 결제중입니다."),
-    ALREADY_RESERVED(HttpStatus.BAD_REQUEST, "이미 예약된 좌석입니다.");
+    ALREADY_RESERVED(HttpStatus.BAD_REQUEST, "이미 예약된 좌석입니다."),
+
+    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 예약정보를 찾을 수 없습니다."),
+
+    NOT_PREOCCUPIED(HttpStatus.BAD_REQUEST, "예약이 선점상태가 아닙니다."),
+    ALREADY_COMPLETED(HttpStatus.BAD_REQUEST, "이미 예약(결제)완료 된 예약입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
