f12f660a41d7017a6bcc1809b4bae7535db93b70
GumaLee <rkdxhd97@naver.com>
2025-02-18 16:40:04 +0900

    TRI-71 예약 취소 검증을 위해 이틀 전인지 검증하는 로직 구현

--- src/main/java/com/trinity/ctc/util/validator/DateTimeValidator.java
+++ src/main/java/com/trinity/ctc/util/validator/DateTimeValidator.java
@@ -28,4 +28,14 @@ public class DateTimeValidator {
             throw new CustomException(DateTimeErrorCode.GIVEN_DATETIME_IS_PAST);
         }
     }
+
+    /**
+     * 날짜 기준으로 이틀포함 이전인지 확인
+     * @param localDate
+     * @return 이틀 전이면 true, 아니면 false
+     */
+    public static boolean isTwoDaysAgoOrBefore(LocalDate localDate) {
+        LocalDate twoDaysAgo = LocalDate.now().minusDays(2);
+        return localDate.isBefore(twoDaysAgo) || localDate.isEqual(twoDaysAgo);
+    }
 }
