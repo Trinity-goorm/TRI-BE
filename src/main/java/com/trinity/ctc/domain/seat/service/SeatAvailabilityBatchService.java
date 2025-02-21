d120ef347238606953ef65f82ba788a21e2674ff
GumaLee <rkdxhd97@naver.com>
2025-02-21 16:00:15 +0900

    TRI-104 환경변수를 통한 가용좌석수 관리 10 -> 1, 스케쥴링 시간 매월 1일 04시

--- src/main/java/com/trinity/ctc/domain/seat/service/SeatAvailabilityBatchService.java
+++ src/main/java/com/trinity/ctc/domain/seat/service/SeatAvailabilityBatchService.java
@@ -37,10 +37,35 @@ public class SeatAvailabilityBatchService {
     private int availableSeatCount;
 
     @Transactional
-    public void batchInsertSeatAvailabilityV1() {
+    public void batchInsertSeatAvailabilityProd() {
         long startTime = System.nanoTime();
 
         List<SeatAvailability> seatAvailabilities = prepareSeatAvailabilityData(DateRangeMode.NEXT_MONTH);
+
+        int totalSize = seatAvailabilities.size();
+        log.info("🚀 INSERT 할 SeatAvailability 데이터 개수: {}", totalSize);
+
+        for (int i = 0; i < totalSize; i++) {
+            entityManager.persist(seatAvailabilities.get(i));
+
+            if ((i + 1) % batchSize == 0) {
+                entityManager.flush();
+                entityManager.clear();
+            }
+        }
+
+        entityManager.flush();
+        entityManager.clear();
+
+        long endTime = System.nanoTime();
+        log.info("✅ 배치 INSERT 완료! 실행 시간: {}ms", (endTime - startTime) / 1_000_000);
+    }
+
+    @Transactional
+    public void batchInsertSeatAvailabilityDummy() {
+        long startTime = System.nanoTime();
+
+        List<SeatAvailability> seatAvailabilities = prepareSeatAvailabilityData(DateRangeMode.TWO_MONTHS);
 
         int totalSize = seatAvailabilities.size();
         log.info("🚀 INSERT 할 SeatAvailability 데이터 개수: {}", totalSize);
@@ -69,12 +94,12 @@ public class SeatAvailabilityBatchService {
         LocalDate startDate;
         LocalDate endDate;
 
-        if (mode == DateRangeMode.CURRENT_MONTH) {
-            startDate = LocalDate.now().withDayOfMonth(1); // 이번 달 1일
-            endDate = startDate.plusMonths(1); // 이번 달 말일까지
-        } else {
+        if (mode == DateRangeMode.NEXT_MONTH) {
             startDate = LocalDate.now().withDayOfMonth(1).plusMonths(1); // 다음 달 1일
             endDate = startDate.plusMonths(1); // 다음 달 말일까지
+        } else {
+            startDate = LocalDate.now().withDayOfMonth(1); // 이번 달 1일
+            endDate = startDate.plusMonths(2); // 다음 달 말일까지
         }
 
         List<SeatAvailability> seatAvailabilities = new ArrayList<>();
