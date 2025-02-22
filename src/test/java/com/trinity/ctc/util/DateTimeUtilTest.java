0353be1a76d15b2e739dfc74d8195ac128391f01
GumaLee <rkdxhd97@naver.com>
2025-02-22 16:16:58 +0900

    TRI-108 예약 취소 COD 로직 테스트

--- src/test/java/com/trinity/ctc/util/DateTimeUtilTest.java
+++ src/test/java/com/trinity/ctc/util/DateTimeUtilTest.java
@@ -3,9 +3,11 @@ package com.trinity.ctc.util;
 import com.trinity.ctc.util.formatter.DateTimeUtil;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.trinity.ctc.util.validator.DateTimeValidator.isMoreThanOneDayAway;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DateTimeUtilTest {
 
@@ -35,5 +37,26 @@ public class DateTimeUtilTest {
         LocalDateTime dateTime = LocalDateTime.of(2025, 2, 10, 15, 45, 30);
         LocalDateTime truncatedMinute = DateTimeUtil.truncateToMinute(dateTime);
         assertEquals(LocalDateTime.of(2025, 2, 10, 15, 45), truncatedMinute);
+    }
+
+    @Test
+    void testIsMoreThanOneDaysAway() {
+        /* 테스트 당시 날짜 = 2025-02-22 */
+
+        // 테스트 대상 날짜들
+        LocalDate reservationDate6 = LocalDate.of(2025, 2, 21);
+        LocalDate reservationDate1 = LocalDate.of(2025, 2, 22);
+        LocalDate reservationDate2 = LocalDate.of(2025, 2, 23);
+        LocalDate reservationDate3 = LocalDate.of(2025, 2, 24);
+        LocalDate reservationDate4 = LocalDate.of(2025, 2, 25);
+        LocalDate reservationDate5 = LocalDate.of(2025, 2, 26);
+
+        // 테스트 실행
+        assertFalse(isMoreThanOneDayAway(reservationDate6));
+        assertFalse(isMoreThanOneDayAway(reservationDate1));
+        assertTrue(isMoreThanOneDayAway(reservationDate2));
+        assertTrue(isMoreThanOneDayAway(reservationDate3));
+        assertTrue(isMoreThanOneDayAway(reservationDate4));
+        assertTrue(isMoreThanOneDayAway(reservationDate5));
     }
 }
