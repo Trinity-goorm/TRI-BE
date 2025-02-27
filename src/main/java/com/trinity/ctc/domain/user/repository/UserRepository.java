f109429c9cba173e4edfc72b966a8215a9df717f
김민철(MInCheol Kim) <ofin0613@naver.com>
2025-02-27 17:13:08 +0900

    TRI-140 사용 안 하는 import 제거

--- src/main/java/com/trinity/ctc/domain/user/repository/UserRepository.java
+++ src/main/java/com/trinity/ctc/domain/user/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.trinity.ctc.domain.user.entity.User;
 
 import java.util.Optional;
 
-import com.trinity.ctc.kakao.dto.KakaoUserInfoResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
