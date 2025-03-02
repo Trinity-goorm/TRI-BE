ea4fdea282e1869e5c751f67f252921fa4d9a36e
김민철(MInCheol Kim) <ofin0613@naver.com>
2025-03-02 12:53:08 +0900

    TRI-140 fcm 등록/삭제 api 분리

--- src/main/java/com/trinity/ctc/domain/fcm/controller/FcmController.java
+++ src/main/java/com/trinity/ctc/domain/fcm/controller/FcmController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,38 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/fcmTokens")
 public class FcmController {
     private final FcmService fcmService;
+
+    @PostMapping("/register")
+    @Operation(
+            summary = "fcm 토큰 등록",
+            description = "최초 로그인 시, fcm 토큰 정보 등록 요청"
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "등록 성공"
+    )
+    public ResponseEntity<Void> registerFcmToken(@RequestBody FcmTokenRequest fcmTokenRequest, HttpRequest request) {
+//        long userId = request.getId();
+//
+//        fcmService.registerFcmToken(fcmTokenRequest, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/delete")
+    @Operation(
+            summary = "fcm 토큰 삭제",
+            description = "로그아웃/로그인 세션 만료 시, fcm 토큰 정보 삭제 요청"
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "삭제 성공"
+    )
+    public ResponseEntity<Void> deleteFcmToken(@RequestBody FcmTokenRequest fcmTokenRequest) {
+//        long userId = request.getId();
+//
+//        fcmService.deleteFcmToken(fcmTokenRequest);
+        return ResponseEntity.noContent().build();
+    }
 
     @PostMapping("/renew")
     @Operation(
