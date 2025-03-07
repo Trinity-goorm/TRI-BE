9525b92b32cf373c26293c4e5fede08d5d408769
GumaLee <rkdxhd97@naver.com>
2025-03-07 09:40:01 +0900

    TRI-189 기존 SeatBatchService 리펙토링

--- src/main/java/com/trinity/ctc/domain/seat/service/SeatBatchService.java
+++ src/main/java/com/trinity/ctc/domain/seat/service/SeatBatchService.java
@@ -7,10 +7,13 @@ import com.trinity.ctc.domain.restaurant.service.RestaurantService;
 import com.trinity.ctc.domain.seat.entity.Seat;
 import com.trinity.ctc.domain.seat.entity.SeatType;
 import com.trinity.ctc.domain.seat.mode.DateRangeMode;
+import com.trinity.ctc.domain.seat.repository.SeatBatchRepository;
+import com.trinity.ctc.domain.seat.repository.SeatRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,12 +27,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SeatBatchService {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final SeatPreparationService seatPreparationService;
 
-    private final RestaurantService restaurantService;
-    private final ReservationTimeService reservationTimeService;
-    private final SeatTypeService seatTypeService;
+    @Qualifier(value = "jpaSeatBatchRepository")
+    private final SeatBatchRepository seatBatchRepository;
 
     @Value("${batch.insert.size:1000}")
     private int batchSize;
@@ -38,82 +39,23 @@ public class SeatBatchService {
 
     @Transactional
     public void batchInsertSeatProd() {
-        long startTime = System.nanoTime();
-
-        List<Seat> seats = prepareSeatData(DateRangeMode.NEXT_MONTH);
-
-        int totalSize = seats.size();
-        log.info("🚀 INSERT 할 Seat 데이터 개수: {}", totalSize);
-
-        for (int i = 0; i < totalSize; i++) {
-            entityManager.persist(seats.get(i));
-
-            if ((i + 1) % batchSize == 0) {
-                entityManager.flush();
-                entityManager.clear();
-            }
-        }
-
-        entityManager.flush();
-        entityManager.clear();
-
-        long endTime = System.nanoTime();
-        log.info("✅ 배치 INSERT 완료! 실행 시간: {}ms", (endTime - startTime) / 1_000_000);
+        executeBatchInsert(DateRangeMode.NEXT_MONTH);
     }
 
     @Transactional
     public void batchInsertSeatDummy() {
+        executeBatchInsert(DateRangeMode.TWO_MONTHS);
+    }
+
+    private void executeBatchInsert(DateRangeMode mode) {
         long startTime = System.nanoTime();
 
-        List<Seat> seats = prepareSeatData(DateRangeMode.TWO_MONTHS);
+        List<Seat> seats = seatPreparationService.prepareSeatsData(mode, availableSeatCount);
+        log.info("🚀 INSERT 할 Seat 데이터 개수: {}", seats.size());
 
-        int totalSize = seats.size();
-        log.info("🚀 INSERT 할 Seat 데이터 개수: {}", totalSize);
-
-        for (int i = 0; i < totalSize; i++) {
-            entityManager.persist(seats.get(i));
-
-            if ((i + 1) % batchSize == 0) {
-                entityManager.flush();
-                entityManager.clear();
-            }
-        }
-
-        entityManager.flush();
-        entityManager.clear();
+        seatBatchRepository.batchInsertSeats(seats, batchSize);
 
         long endTime = System.nanoTime();
         log.info("✅ 배치 INSERT 완료! 실행 시간: {}ms", (endTime - startTime) / 1_000_000);
-    }
-
-    private List<Seat> prepareSeatData(DateRangeMode mode) {
-        List<Restaurant> restaurants = restaurantService.getAllRestaurants();
-        List<ReservationTime> reservationTimes = reservationTimeService.getAllReservationTimes();
-        List<SeatType> seatTypes = seatTypeService.getAllSeatTypes();
-
-        LocalDate startDate;
-        LocalDate endDate;
-
-        if (mode == DateRangeMode.NEXT_MONTH) {
-            startDate = LocalDate.now().withDayOfMonth(1).plusMonths(1); // 다음 달 1일
-            endDate = startDate.plusMonths(1); // 다음 달 말일까지
-        } else {
-            startDate = LocalDate.now().withDayOfMonth(1); // 이번 달 1일
-            endDate = startDate.plusMonths(2); // 다음 달 말일까지
-        }
-
-        List<Seat> seats = new ArrayList<>();
-
-        for (LocalDate reservationDate = startDate; reservationDate.isBefore(endDate); reservationDate = reservationDate.plusDays(1)) {
-            for (Restaurant restaurant : restaurants) {
-                for (ReservationTime reservationTime : reservationTimes) {
-                    for (SeatType seatType : seatTypes) {
-                        seats.add(Seat.create(restaurant, reservationDate, reservationTime, seatType, availableSeatCount));
-                    }
-                }
-            }
-        }
-
-        return seats;
     }
 }
