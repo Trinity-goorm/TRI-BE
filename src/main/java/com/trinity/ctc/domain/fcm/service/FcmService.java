f8d7e84ad05670ca8984441a6b0463300ae9ba89
김민철(MInCheol Kim) <ofin0613@naver.com>
2025-03-10 11:29:40 +0900

    TRI-193 fcm 토큰 관련 api 수정

--- src/main/java/com/trinity/ctc/domain/fcm/service/FcmService.java
+++ src/main/java/com/trinity/ctc/domain/fcm/service/FcmService.java
@@ -28,10 +28,10 @@ public class FcmService {
     /**
      * 로그인 시, 해당 기기에 대한 사용자의 fcm 토큰 정보 초기화
      *
-     * @param fcmTokenRequest FCM토큰 정보 요청 DTO(토큰값, 등록 시간)
+     * @param fcmToken fcm 토큰 값
      */
     @Transactional
-    public void registerFcmToken(FcmTokenRequest fcmTokenRequest) {
+    public void registerFcmToken(String fcmToken) {
         String kakaoId = authService.getAuthenticatedKakaoId();
         
         // 유저 entity
@@ -44,7 +44,7 @@ public class FcmService {
 
         // FCM 토큰 entity 빌드
         Fcm fcm = Fcm.builder()
-                .token(fcmTokenRequest.getFcmToken())
+                .token(fcmToken)
                 .registeredAt(registeredAt)
                 .expiresAt(expiresAt)
                 .user(user)
@@ -57,22 +57,20 @@ public class FcmService {
     /**
      * 로그아웃 시, 해당 기기에 대한 사용자의 fcm 토큰 정보 삭제
      *
-     * @param fcmTokenRequest FCM토큰 정보 요청 DTO(토큰값, null)
+     * @param fcmToken fcm 토큰 값
      */
     @Transactional
-    public void deleteFcmToken(FcmTokenRequest fcmTokenRequest) {
-        fcmRepository.deleteByToken(fcmTokenRequest.getFcmToken());
+    public void deleteFcmToken(String fcmToken) {
+        fcmRepository.deleteByToken(fcmToken);
     }
 
     /**
      * 로그인 세션이 유지된 상태에서 접속 시, fcm 토큰 만료 기간 갱신
      *
-     * @param fcmTokenRequest FCM토큰 정보 요청 DTO(토큰값, 업데이트 시간)
+     * @param fcmToken fcm 토큰 값
      */
     @Transactional
-    public void renewFcmToken(FcmTokenRequest fcmTokenRequest) {
-        String fcmToken = fcmTokenRequest.getFcmToken();
-
+    public void renewFcmToken(String fcmToken) {
         // 토큰 업데이트 시간과 만료 시간 설정
         LocalDateTime updatedAt = DateTimeUtil.truncateToMinute(LocalDateTime.now());
         LocalDateTime expiresAt = updatedAt.plusDays(30);
