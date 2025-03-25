1b79ae6021d433e7d617442b51c934f463658033
김민철(MInCheol Kim) <ofin0613@naver.com>
2025-03-25 10:06:11 +0900

    TRI-223 발송 스레드 커스텀

--- src/main/java/com/trinity/ctc/global/firebase/CustomThreadManager.java
+++ src/main/java/com/trinity/ctc/global/firebase/CustomThreadManager.java
@@ -3,15 +3,31 @@ package com.trinity.ctc.global.firebase;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.ThreadManager;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.*;
 
 public class CustomThreadManager extends ThreadManager {
 
     @Override
     protected ExecutorService getExecutor(FirebaseApp firebaseApp) {
-        return Executors.newFixedThreadPool(100);
+        ThreadFactory threadFactory = runnable -> {
+            Thread thread = new Thread(runnable);
+            thread.setName("firebase-msg-" + thread.getId());
+            thread.setDaemon(false);
+            thread.setPriority(Thread.MAX_PRIORITY);
+            return thread;
+        };
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                100,
+                100,
+                60L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(),
+                threadFactory
+        );
+
+        executor.prestartAllCoreThreads();
+
+        return executor;
     }
 
     @Override
