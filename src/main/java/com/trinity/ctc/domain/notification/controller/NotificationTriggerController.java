bc713536193cb47346c928a787d3198735827d8e
김민철(MInCheol Kim) <ofin0613@naver.com>
2025-03-31 20:52:14 +0900

    Fix - 알림 trigger api 경로 추가

--- src/main/java/com/trinity/ctc/domain/notification/controller/NotificationTriggerController.java
+++ src/main/java/com/trinity/ctc/domain/notification/controller/NotificationTriggerController.java
@@ -10,37 +10,37 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/trigger/notifications")
+@RequestMapping("/api/notifications")
 public class NotificationTriggerController {
 
     private final TestNotificationService testNotificationService;
 
     // 1. 빈자리 알림
-    @PostMapping("/send/seat")
+    @PostMapping("trigger/send/seat")
     public void sendSeatNotification(@RequestParam long seatId) {
         testNotificationService.sendSeatNotification(seatId);
     }
 
     // 2. 당일 예약 알림
-    @PostMapping("/send/daily")
+    @PostMapping("trigger/send/daily")
     public void sendDailyNotification(@RequestParam String date) {
         testNotificationService.sendDailyNotification(date);
     }
 
     // 3. 한 시간 전 예약 알림
-    @PostMapping("/send/hourly")
+    @PostMapping("trigger/send/hourly")
     public void sendHourlyNotification(@RequestParam String dateTime) {
         testNotificationService.sendHourBeforeNotification(dateTime);
     }
 
     // 4. 예약 완료 알림
-    @PostMapping("/send/complete")
+    @PostMapping("trigger/send/complete")
     public void sendReservationComplete(@RequestParam long userId, @RequestParam long reservationId) {
         testNotificationService.sendReservationCompletedNotification(userId, reservationId);
     }
 
     // 5. 예약 취소 알림
-    @PostMapping("/send/cancel")
+    @PostMapping("trigger/send/cancel")
     public void sendReservationCancel(@RequestParam long userId, @RequestParam long reservationId) {
         testNotificationService.sendReservationCanceledNotification(userId, reservationId);
     }
